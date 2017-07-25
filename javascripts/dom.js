6d08844cfd3eeec51819d9e5f50ed789f2b01990
Casey Dailey <casey@feasttogether.org>
2017-07-24 22:34:34 -0500

    got the damn radio button value

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -4,10 +4,28 @@
 //grab elements
 let table = document.querySelector("#table");
 let form = document.querySelector("#form");
+let inputs = Array.from(document.querySelectorAll(".input"));
+let lap = document.querySelector("#lap");
+let lump = document.querySelector("#lump");
+let radios = Array.from(document.querySelectorAll(".radio"));
+
+
 
 //add listeners
-form.addEventListener("submit", donate);
+form.addEventListener("submit", getData);
+
+function getData(event){
+    event.preventDefault();
+    inputs.forEach((input)=>{
+        console.log("input.name",input.name);
+        console.log(`${input.name}.value`,input.value);
+    });
+    // console.log("checked: ", checked);
+    console.log("radios: ", radios);
+    let checked = radios.filter((radio)=>radio.checked === true);
+    console.log("checked:", checked);
+}
 
 function donate(event) {
-    console.log("submit event: ", event);
+    
 }
